09e1fff10929194a06963b15ef3d30efe78460e6
ellies09 <ellies09@gmail.com>
2026-09-10 16:04:17 +0100

    Update cache version and asset references

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'breeze-web-v16';
-const ASSETS = ['./', './index.html', './app.js?v=16', './tink-hpke.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+const CACHE = 'breeze-web-v17';
+const ASSETS = ['./', './index.html', './app.js?v=17', './tink-hpke.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
