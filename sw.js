1045f24bb4f1f596aa39ad7edd306717884e94c1
ellies09 <ellies09@gmail.com>
2026-09-10 09:51:27 +0100

    Bump cache version to v14

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'breeze-web-v13';
-const ASSETS = ['./', './index.html', './app.js?v=13', './tink-hpke.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+const CACHE = 'breeze-web-v14';
+const ASSETS = ['./', './index.html', './app.js?v=14', './tink-hpke.js', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
